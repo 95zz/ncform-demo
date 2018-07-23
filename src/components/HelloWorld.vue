38b74a6c6cd8ea774d75cf3bf7247bb930ac6046
daniel-dx <danieldx666@126.com>
2018-07-23 20:41:10 +0800

    add project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ncform :form-schema="formSchema" form-name="your-form-name" v-model="formSchema.value" @submit="submit()"></ncform>
+    <el-button @click="submit()">Submit</el-button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      formSchema: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    submit () {
+      this.$ncformValidate('your-form-name').then(data => {
+        if (data.result) {
+          console.log(this.$data.formSchema.value)
+          // do what you like to do
+        }
+      })
+    }
+  }
+}
+</script>
